83d1dba329230a7048a314795eff4e8242dcf746
Romário Ferreira <romarioferreirarezende@hotmail.com>
2021-09-12 00:13:17 -0300

    implemented Pageable

--- src/main/java/com/devsuperior/aulajparepository/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/aulajparepository/repositories/UserRepository.java
@@ -1,10 +1,22 @@
 package com.devsuperior.aulajparepository.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.aulajparepository.entitie.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+
+
+	@Query("SELECT obj FROM User obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))")
+	Page<User> SearchName(String name, Pageable pageable);
+	
+	Page<User> findBySalaryBetween(Double minSalary, Double maxSalary, Pageable pageable);
+	
+	Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
 	
 	
 
